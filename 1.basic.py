b6744e36e2d04bd31122b857c03f9b339d6e95c0
zhenxu <zhenxu66@gmail.com>
2019-06-05 11:12:42 -0700

    Initial commit

---
+++ 1.basic.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Test Window")
+root.geometry('200x500')
+
+
+
+# ------------Label-----------------
+# object first capitalized, width & height by char
+# label1 = tk.Label(root, text='Trial', bg='red', font=('Arial', 12), width=15, height=3)
+text_var = tk.StringVar()
+
+label1 = tk.Label(root, textvariable=text_var, bg='red', font=('Arial', 12), width=15, height=3)
+label1.pack()  # put in next place or place
+
+# ------------Button-----------------
+on_hit = False
+def func_hit():
+    global on_hit
+    if on_hit == False:
+        on_hit = True
+        text_var.set('Hit the button')
+    else:
+        on_hit = False
+        text_var.set('')
+
+botton1 = tk.Button(root, text='hit me', width=15, height=2, command=func_hit)
+botton1.pack()
+
+# ------------Entry--Text--------
+entry1 = tk.Entry(root, show='*')
+entry1.pack()
+text1 = tk.Text(root, height=2)
+text1.pack()
+
+def insert_point():
+    var = entry1.get()
+    text1.insert('insert', var)
+
+def insert_end():
+    var = entry1.get()
+    text1.insert('end', var)
+
+
+button2_1 = tk.Button(root, text='insert point', width=15, height=2, command=insert_point)
+button2_1.pack()
+
+button2_2 = tk.Button(root, text='insert end', width=15, height=2, command=insert_end)
+button2_2.pack()
+
+button2_3 = tk.Button(root, text='clear', width=15, height=2, command=lambda: text1.delete(1.0, tk.END))
+button2_3.pack()
+
+# ------------Listbox--------
+
+text_var2 = tk.StringVar()
+
+label2 = tk.Label(root, textvariable=text_var2, bg='yellow', font=('Arial', 12), width=6, height=3)
+label2.pack()
+
+
+var2 = tk.StringVar()
+var2.set((11, 22, 33, 'last'))  # 为变量设置值
+listbox1 = tk.Listbox(root, height=6, listvariable=var2)  #将var2的值赋给Listbox
+
+# 创建一个list并将值循环添加到Listbox控件中
+# list_items = [1,2,3,4]
+# for item in list_items:
+#     listbox1.insert('end', item)  #从最后一个位置开始加入值
+listbox1.insert(0, 'first')       #在第一个位置加入'first'字符
+listbox1.insert(1, 'second')      #在第二个位置加入'second'字符
+listbox1.delete(1)                #删除第二个位置的字符
+listbox1.pack()
+
+
+def print_from_list():
+    value = listbox1.get(listbox1.curselection())   # 获取当前选中的文本 curselection
+    text_var2.set(value)     # 为label设置值
+
+button3 = tk.Button(root, text='print selection', width=15, height=2, command=print_from_list)
+button3.pack()
+
+# like while to refresh window
+
+root.mainloop()
